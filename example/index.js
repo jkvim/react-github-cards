36cddf71c71ff86227e1ad98e6a463108df5e48c
jkvim <jkvim@outlook.com>
2016-08-10 19:38:08 +0800

    example: update view

--- example/index.js
+++ example/index.js
@@ -1,13 +1,80 @@
+"use strict";
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { UserCard, RepoCard } from '../src/themes/medium';
+import * as Medium from '../src/themes/medium';
+import * as Default from '../src/themes/default';
+import './index.scss';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      theme: 'default',
+      username: 'jkvim',
+      repo: 'vivid.js'
+    };
+    this.themes = {
+      'default': Default,
+      'medium': Medium
+    };
+    this.onThemeChange = this.onThemeChange.bind(this);
+    this.onSetState = this.onSetState.bind(this);
+    this.onResetState = this.onResetState.bind(this);
+  }
+  onThemeChange(evt) {
+    this.setState({
+      theme: evt.target.value
+    });
+  }
+  onSetState(state) {
+    return () => {
+      if (state in this.state) {
+        this.setState({
+          [state]: this.refs[state].value
+        });
+      }
+    }
+  }
+  onResetState(state) {
+    return () => {
+      if (state in this.state) {
+        this.setState({
+          [state]: ''
+        });
+      }
+    }
+  }
   render() {
+    const { UserCard, RepoCard } = this.themes[this.state.theme];
     return (
       <div id="root">
-        <UserCard username={'jkvim'} />
-        <RepoCard username={'jkvim'} repo={'vivid.js'} />
+        <h1>Choose Theme</h1>
+        <div className="theme-select">
+          <select onChange={this.onThemeChange}>
+            <option value="default">default</option>
+            <option value="medium">medium</option>
+          </select>
+        </div>
+        <div className="github-cards">
+          <div className="input-wrapper">
+            <input
+              ref="username"
+              placeholder="username"
+              onBlur={this.onSetState('username')}
+              onChange={this.onResetState('username')} />
+            <input
+              ref="repo"
+              placeholder="repo"
+              onBlur ={this.onSetState('repo')}
+              onChange={this.onResetState('repo')} />
+          </div>
+          {this.state.username ?
+            <UserCard username={this.state.username} /> : ''
+          }
+          {this.state.repo ?
+            <RepoCard username={this.state.username} repo={this.state.repo} />: ''
+          }
+        </div>
       </div>
     );
   }
